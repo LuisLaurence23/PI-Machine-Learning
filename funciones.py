1dde9bdd3c341c1fc8a3df200be1a8ca7a706ef2
unknown <llaurencesalas@gmail.com>
2023-10-30 10:59:31 -0700

    Last commt

--- funciones.py
+++ funciones.py
@@ -6,101 +6,133 @@ import pickle
 #Lectura de archivos
 df_games = pd.read_parquet('./data/steam_games22.parquet')
 df_reviews = pd.read_parquet('./data/user_reviews.parquet')
-df_machine= pd.read_parquet('./data/df_ML2.parquet')
 #df_items = pd.read_parquet('./data/user_items.parquet')
 
 
-#UserForGenrep(genero)
-#df_marge_item = pd.merge(df_games , df_items,on="item_id" )
-#df_nuevo = df_marge_item.drop(columns=["item_id","app_name","price","developer","items_count","item_name","playtime_2weeks"])
-
-#best_developer_yearp(year)
-merged_df = pd.merge(df_reviews, df_games, on='item_id')
-merged_df = merged_df.rename(columns={'posted_year': 'year'})
-merged_dff=merged_df[['year','recommend','sentiment_analysis','developer','app_name']]
+#Dataframes usados para el punto 1
+#df_games = pd.read_parquet('./data/steam_games22.parquet')
 
 
-#userdata2(user_id)
+#Dataframes usados para el punto 2
 merged_reviews_games = df_reviews.merge(df_games[['item_id', 'price']])
 merged_reviews_games.drop(columns=['helpful','posted_year',"sentiment_analysis"], inplace=True)
 merged_reviews_games['item_id'] = merged_reviews_games['item_id'].astype('int32')
 
 
-#developer_reviews_analysis(desarrollador)
+#Dataframes usados para el punto 3
+#df_marge_item = pd.merge(df_games , df_items,on="item_id" )
+#df_nuevo = df_marge_item.drop(columns=["item_id","app_name","price","developer","items_count","item_name","playtime_2weeks"])
+
+
+#Dataframes usados para el punto 4
+merged_df = pd.merge(df_reviews, df_games, on='item_id')
+merged_df = merged_df.rename(columns={'posted_year': 'year'})
+merged_dff=merged_df[['year','recommend','sentiment_analysis','developer','app_name']]
+
+
+#Dataframes usados para el punto 5
 analisis_f= merged_df[['developer','sentiment_analysis']]
 
 
+#Dataframes usados para Machine Learning
+df_machine= pd.read_parquet('./data/df_ML2.parquet')
 
-#1
+
+
+#----------------------------------------------------------------Función1----------------------------------------------------------------
+
 def developer(desarrollador):
+    
+    #Filtramos el df de juegos con el desarrollador especificado
     df_developer = df_games[df_games["developer"] == desarrollador]
+    
+    #Se obtiene la cantidad de items por año de lanzamiento
     items_year = df_developer.groupby("release_year")["item_id"].count()
 
-    # Se filtra el df del desarrolladorpara aquellos juegos gratuitos (precio cero):
+    #Filtramos los juegos gratuitos (price = 0)
     df_dev_free = df_developer[df_developer["price"] == 0] 
 
-    # Se obtiene la cantidad de items gratuitos por años
-    free_items = df_dev_free.groupby("release_year")["price"].count() #cantidad de gratis por año 
+    #Cantidad de juegos gratuitos por años
+    free_items = df_dev_free.groupby("release_year")["price"].count() 
 
-    # Se calcula el porcentaje de contenido gratuito por año
+    #Porcentaje de contenido gratuito por año
     free_proportion = round((free_items / items_year) * 100, 2)
 
-    # Se asigna nombre a las series para poder unirlas en un dataframe:
+    #Creamos el nombre a las series para unirlas en un df:
     items_year.name = "Cantidad de Items"
     free_proportion.name = "Contenido Free"
 
-    df1 = pd.merge(items_year, free_proportion, on = "release_year").reset_index()
-    df1 = df1.fillna(0)
+    
+    df1 = pd.merge(items_year, free_proportion, on = "release_year").reset_index() #se unen las series en un df
+    df1 = df1.fillna(0)                                                            #llenamos NaN con 0
+    df1 = df1.rename(columns={"release_year" : "Año"})                             #renombre de columna release_year por Año
 
-    df1 = df1.rename(columns={"release_year" : "Año"})
 
-    # Se da formato a la columna de contenido free:
-    df1["Contenido Free"] = df1["Contenido Free"].apply(lambda x: f"{x}%")
+    #Agregamos el simbolo de porcentaje
+    df1["Contenido Free"] = df1["Contenido Free"].apply(lambda x: f"{x}%") 
 
-    # Se convierte el df en diccionario
+    #orient="records" para que el diccionario sea de la forma: [{columna -> valor}]
     diccionario = df1.to_dict(orient="records")
+    
+    #Se eliminan los df que ya no se usan en la funcion
     del df_developer, items_year, df_dev_free, free_items, free_proportion, df1
+    
     return diccionario
 
+#----------------------------------------------------------------Función2----------------------------------------------------------------
 
-#2
 def userdata2(user_id):
-    # Filtrar los datos para el usuario especificado
+    #Filtramos los datos para el usuario especificado
     user_data = merged_reviews_games[merged_reviews_games['user_id'] == user_id]
 
-    #user_items = user_data[user_data['user_id'] == user_id]
-
-    # Calcular la cantidad de dinero gastado por el usuario
+    #Suma de dinero gastado por el usuario
     dinero_gastado = user_data['price'].sum()
 
-    # Calcular el porcentaje de recomendación en base a reviews.recommend
+    #Suma de recomendaciones positivas por el usuario (recommend = True)
     recomendacion = (user_data['recommend'] == True).sum()
     porcentaje_recomendacion = recomendacion / len(user_data) * 100
 
-    # Calcular la cantidad de items
-    cantidad_de_items = user_data['item_id'].nunique()
+    #Cantidad de items
+    cantidad_de_items = user_data['item_id'].nunique()#
 
-    # Crear un diccionario con los resultados
+    #Diccionario con los resultados
     resultados = {
         'Cantidad de dinero gastado': dinero_gastado,
         'Porcentaje de recomendación': porcentaje_recomendacion,
         'Cantidad de items': cantidad_de_items
     }
+    
+    #Se eliminan los df que ya no se usan en la funcion
     del user_data, dinero_gastado, recomendacion, porcentaje_recomendacion, cantidad_de_items
+    
     return resultados
 
 
+#----------------------------------------------------------------Función3----------------------------------------------------------------
 
-#3
-'''
+#En esta función en especifico, tuve algunos problemas al realizar el deploy en render por cuestiones de memoria. El df_items tiene
+#aproximadamente 5 millones de registros, por lo que al momento de realizar el deploy, el servidor mandaba un error por memoria.
+#Por lo tanto la dejo comentada para no tener ese problema. Solamente me da el resultado de manera local en la API.
+
+''' 
 def UserForGenrep(genero):
-    df_genre = df_nuevo[df_nuevo[genero] == 1] #llamo el genero
-    usur_mas_horas = df_genre.groupby("user_id")["playtime_forever"].sum().idxmax() #usuario con más horas de juego en el genero
-    filtro_usur = df_genre[df_genre["user_id"] == usur_mas_horas] #filtramos por el usuario con mas horas jugadas
-    horas_jugXaño = filtro_usur.groupby("release_year")["playtime_forever"].sum() #horas jugadas por año 
 
-    registro = horas_jugXaño.to_dict() #paso las horas por año en diccionario
-    #del registro[0] #elimino el registro 0 que no me sirve
+    #filtramos donde el genero sea 1 (True)
+    df_genre = df_nuevo[df_nuevo[genero] == 1] 
+    
+    #idxmax() devuelve el indice del valor maximo del usuario con mas horas jugadas en el género
+    usur_mas_horas = df_genre.groupby("user_id")["playtime_forever"].sum().idxmax() 
+    
+    #Filtramos por el usuario con mas horas jugadas
+    filtro_usur = df_genre[df_genre["user_id"] == usur_mas_horas]
+    
+    #Agrupamos por año y sumamos las horas jugadas
+    horas_juego_x_año = filtro_usur.groupby("release_year")["playtime_forever"].sum()
+
+    #Pasamos a diccionario
+    registro = horas_juego_x_año.to_dict()
+    
+    #Diccionario con los resultados
     Horas_por_año = {}
     for clave, valor in registro.items():
         clave_formateada = f'Año: {int(clave)}'
@@ -108,76 +140,99 @@ def UserForGenrep(genero):
         Horas_por_año[clave_formateada] = valor_formateado
 
     return {"Usuario con más horas jugadas": usur_mas_horas, "Horas jugadas por año": Horas_por_año}
-
 '''
 
+#----------------------------------------------------------------Función4----------------------------------------------------------------
 
-#4
 def best_developer_yearp(year):
+    #Filtramos por año especificado en la función
     df_year = merged_dff[merged_dff['year'] == year]
+    
+    #Aplicamos un filtro para obtener los juegos recomendados y con sentimiento positivo, agrupamos por desarrollador y contamos la cantidad de juegos
     df_count = df_year.loc[(df_year['recommend'] == True) & (df_year['sentiment_analysis'] == 2)].groupby('developer')['app_name'].count().reset_index()
+    
+    #Ordenamos de mayor a menor y tomamos los 3 primeros y los guardamos en una lista
     top_desarrolladores = df_count.sort_values('app_name', ascending=False).head(3)['developer'].tolist()
     
-    # Crear la lista de diccionarios
+    #Creamos lista de diccionarios para la respuesta
     resultados = {f'Puesto {i+1}': desarrollador for i, desarrollador in enumerate(top_desarrolladores)}
+    
     return resultados
 
-#5
+
+
+#----------------------------------------------------------------Función5----------------------------------------------------------------
+
 def developer_reviews_analysis(desarrolladora):
-    desarrolladores=analisis_f[analisis_f['developer'] == desarrolladora]
-    df_positivas = desarrolladores[desarrolladores['sentiment_analysis'] == 2]
-    df_negativas = desarrolladores[desarrolladores['sentiment_analysis'] == 0]
+    #Filtramos por desarrolladora especificada en la función
+    desarrolladores = analisis_f[analisis_f['developer'] == desarrolladora]
     
+    #Filtramos por sentimiento positivo(2) y negativo(0)
+    df_positivas    = desarrolladores[desarrolladores['sentiment_analysis'] == 2]
+    df_negativas    = desarrolladores[desarrolladores['sentiment_analysis'] == 0]
+    
+    #Obtenemos la cantidad de reviews positivas y negativas
     cantidad_positivas = len(df_positivas)
     cantidad_negativas = len(df_negativas)
 
+    #Creamos un diccionario con los resultados
     resultados = {
         desarrolladora: {
             'positivas': cantidad_positivas,
             'negativas': cantidad_negativas
         }
     }
+    
+    #Se eliminan los df que ya no se usan en la funcion
     del desarrolladores, df_positivas, df_negativas, cantidad_positivas, cantidad_negativas
+    
+    #Retornamos el diccionario
     return resultados
 
 
 
 
+#----------------------------------------------------------------Machine Learning---------------------------------------------------------
 
 
+#Traemos el modelo ya entrenado con ayuda del modulo pickle
 with open ('./data/modelo2.pkl', 'rb') as file:
     modelo=pickle.load(file)
 
 def recomendar_peliculas(user_id):
     
+    #Creamos una lista vacía para guardar los juegos recomendados, y un diccionario para guardar el
+    #retorno final con clave-valor
+    
     listado=[]
     diccionario = {}
-    # Crear una lista de juegos ya valorados por el usuario específico
+    
+    #Juegos ya valorados por el usuario que se pasa como parámetro 
     juegos_valorados = df_machine[df_machine['user_id'] == user_id]['app_name'].unique()
 
-    # Crear una lista de todos los juegos disponibles
+    #Juegos disponibles
     todos_los_juegos = df_machine['app_name'].unique()
 
-    # Crear una lista de juegos no valorados por el usuario específico
+    #Juegos no valorados por el usuario (para no recomendarle los mismos)
     juegos_no_valorados = list(set(todos_los_juegos) - set(juegos_valorados))
 
-    # Generar predicciones para los juegos no valorados por el usuario
+    #Hacemos predict para los juegos no valorados por el usuario
     predicciones = [modelo.predict(user_id, juego) for juego in juegos_no_valorados]
 
-    # Ordenar las predicciones en base a la valoración y obtener los juegos recomendados
-    recomendaciones = sorted(predicciones, key=lambda x: x.est, reverse=True)[:5]  # Obtener las 5 mejores recomendaciones
+    #Ordenamos las predicciones respecto a la valoración y tomamos los 5 mejos juegos recomendados
+    recomendaciones = sorted(predicciones, key=lambda x: x.est, reverse=True)[:5]
 
-    # Mostrar los juegos recomendados
-    
+
+    #Guardamos los juegos recomendados en una lista 
     for recomendacion in recomendaciones:
-        #print(f"Juego: {recomendacion.iid}")
         listado.append(recomendacion.iid)
-        
-    
+     
+    #Creamos un diccionario con clave-valor para retornar los juegos recomendados   
     for i, juego in enumerate(listado, start=1):
         opcion = f"opcion{i}"
         diccionario[opcion] = juego
 
+    #Si el usuario no existe, se muestra el mensaje "El usuario no existe"
     if user_id not in df_machine['user_id'].unique():
         return "El usuario no existe"
     else:
--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 
 from fastapi import FastAPI
 from funciones import developer_reviews_analysis
-#from funciones import UserForGenrep
+#from funciones import UserForGenrep (Funcion 3 que usa el df_items)
 from funciones import best_developer_yearp
 from funciones import userdata2
 from funciones import developer
 from funciones import recomendar_peliculas
 
 
-app = FastAPI()
+# Instanciamos FastAPI
+app = FastAPI() 
+
+#--------------------------------------------------------Función1---------------------------------------------------------------------------
+
+#Cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora
+
+@app.get("/developer/{desarrollador}")       #Hacemos el endpoint
+async def developer_def(desarrollador: str): #Definimos la función, el parámetro es el nombre del desarrollador
+    result2 = developer(desarrollador)       #Llamamos a la función developer y le pasamos el parámetro
+    return result2                           #Retornamos el resultado de la función
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-#1
-@app.get("/developer/{desarrollador}")
-async def developer_def(desarrollador: str):
-    result2 = developer(desarrollador)
-    return result2 
 
 
-#2
-@app.get("/userdata/{user_id}")
-async def userdata_handler(user_id: str):
-    try:
-        result3 = userdata2(user_id)
-        return result3
-    except Exception as e:
-        return {"error": str(e)}
+#--------------------------------------------------------Función2---------------------------------------------------------------------------
+
+#Devuelve la cantidad de dinero gastado por el usuario, el porcentaje de recomendación en base a reviews.recommend y cantidad de items
+
+@app.get("/userdata/{user_id}")             #Hacemos el endpoint
+async def userdata_handler(user_id: str):   #Definimos la función, el parámetro es el id del usuario
+    try:                                    #Intentamos ejecutar la función
+        result3 = userdata2(user_id)        #Llamamos a la función userdata2 y le pasamos el parámetro
+        return result3                      #Retornamos el resultado de la función
+    except Exception as e:                  #Si hay un error, lo capturamos
+        return {"error": str(e)}            #Retornamos el error
 
 
-#3
+
+
+#-------------------------------------------------------Función3-----------------------------------------------------------------------------
 ''' 
+Devuelve el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año de lanzamiento
+ 
 @app.get("/UserForGenre/{genero}")
 async def UserForGenre(genero: str):
     try:
@@ -43,25 +51,38 @@ async def UserForGenre(genero: str):
         return {"error": str(e)}
     '''
     
-#4
-@app.get("/best_developer_year/{year}")
-async def Best_developer_year(year: str):
-    try:
-        year_int = int(year)  # Convertir el año a un entero
-        result2 = best_developer_yearp(year_int)
-        return result2
-    except Exception as e:
-        return {"error": str(e)}
+#-------------------------------------------------------Función4-----------------------------------------------------------------------------
+
+'''Devuelve el top 3 de desarrolladores con juegos más recomendados por usuarios para el año dado.
+   (reviews.recommend = True y comentarios positivos).'''
+
+@app.get("/best_developer_year/{year}")             #Hacemos el endpoint
+async def Best_developer_year(year: str):           #Definimos la función, el parámetro es el año
+    try:                                            #Intentamos ejecutar la función
+        year_int = int(year)                        #Convertimos el año a entero
+        result2 = best_developer_yearp(year_int)    #Llamamos a la función best_developer_yearp y le pasamos el parámetro
+        return result2                              #Retornamos el resultado de la función
+    except Exception as e:                          #Si hay un error, lo capturamos
+        return {"error": str(e)}                    #Retornamos el error
 
 
-#5
-@app.get("/developer_reviews_analysis/{desarrollador}")
-async def developer_def(desarrollador: str):
-    result2 = developer_reviews_analysis(desarrollador)
-    return result2 
 
 
-#Machine Learning id_usuario
+#-------------------------------------------------------Función5-------------------------------------------------------------------------------
+
+'''Según el desarrollador, se devuelve un diccionario con el nombre del desarrollador como llave y una lista con la cantidad total de registros
+   de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor positivo o negativo.'''
+
+@app.get("/developer_reviews_analysis/{desarrollador}")  #Hacemos el endpoint
+async def developer_def(desarrollador: str):             #Definimos la función, el parámetro es el nombre del desarrollador
+    result2 = developer_reviews_analysis(desarrollador)  #Llamamos a la función developer_reviews_analysis y le pasamos el parámetro
+    return result2                                       #Retornamos el resultado de la función
+
+
+
+#-------------------------------------------------------Machine Learning------------------------------------------------------------------------
+
+'''Ingresando el id de un usuario, recibimos una lista con 5 juegos recomendados para dicho usuario.'''
 @app.get("/recomendacion_usuario/{id_usuario}")
 async def recomendacion_usuario(id_usuario: str):
     result = recomendar_peliculas(id_usuario)
